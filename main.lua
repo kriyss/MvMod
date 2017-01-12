da85f537c3bbc02f0cfa20393f57bfee3cf75cda
kriyss <perso@kriyss.ninja>
2017-01-12 21:45:39 +0100

    feat: add tequito effect

--- main.lua
+++ main.lua
@@ -2,23 +2,30 @@ local MvMod = RegisterMod("MvMod", 1);
 local Tequito = Isaac.GetItemIdByName("Tequito");
 
 local count = 0
+local damageUp = 0.4
+local upMax = 5
 local done = false
 
 function MvMod:UseTequito()
     local player = Isaac.GetPlayer(0)
     if done then 
+        player.Damage = player.Damage - (damageUp * upMax)
+        Isaac.Spawn( EntityType.ENTITY_EFFECT, EffectVariant.PLAYER_CREEP_LEMON_PARTY, 0, player.Position, Vector( 0.0, 0.0 ), player );
         player:RemoveCollectible(Tequito)
         player:AnimateSad()
+        count = 0
+        done = false
         return 
     end 
     
-    player.Damage = player.Damage + 1
-    player.FireDelay = player.FireDelay + 1
+    player.Damage = player.Damage + damageUp
     count = count + 1
+    
 
-    if count >= 5 then 
+    if count >= upMax then 
         player:AnimateHappy()
         done = true 
+
     end
 end
 
